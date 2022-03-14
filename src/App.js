545d4d813aec2fca7013c19cd280862f8352e3da
dorut <dorut.iuga@inventiff.co>
2022-03-14 09:18:24 +0200

    update with router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./page/homepage/homepage.jsx";
+
+const Hats = () => <div>hats page</div>;
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop/hats" component={Hats} />
+      </Switch>
     </div>
   );
 }
