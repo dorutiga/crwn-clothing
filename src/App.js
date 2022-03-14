15f55a20c782c88429a223daa07fcdd611c3f37c
dorut <dorut.iuga@inventiff.co>
2022-03-14 10:31:55 +0200

    added shop page with styling

--- src/App.js
+++ src/App.js
@@ -2,8 +2,12 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./page/homepage/homepage.jsx";
+import ShopPage from "./page/shop/shop-page.component";
 
-const Hats = () => <div>hats page</div>;
+const Hats = (props) => {
+  console.log(props);
+  return <div>hats page</div>;
+};
 
 function App() {
   return (
@@ -11,6 +15,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop/hats" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
